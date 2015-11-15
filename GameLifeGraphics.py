eb0d7a3a0f7736b333f9e16c620dbd56b23695e9
Nick Cellino <nacellin@buffalo.edu>
2015-11-14 20:47:57 -0500

    working on music stuff

--- GameLifeGraphics.py
+++ GameLifeGraphics.py
@@ -5,14 +5,15 @@ from SquareGridUI import SquareGridUI
 from colors import *
 
 resolution = (800, 800)
+board_size = 40
 
 # Global initialization
 pygame.init()
 pygame.display.set_caption("Game of Life")
 DISPLAYSURF = pygame.display.set_mode(resolution, RESIZABLE)
 
-the_grid = SquareGridUI(DISPLAYSURF, 15, resolution[0])
-the_game = GameOfLife(15, 15)
+the_grid = SquareGridUI(DISPLAYSURF, board_size, resolution[0])
+the_game = GameOfLife(board_size, board_size)
 the_game.random_grid_init()
 
 while True:
@@ -20,8 +21,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    DISPLAYSURF.fill(BLACK)
+    DISPLAYSURF.fill(RED)
     the_grid.draw_grid((0,0), the_game.grid)
-    pygame.time.wait(200)
+    pygame.time.wait(100)
     the_game.time_step()
     pygame.display.update()
